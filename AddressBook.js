473ac73f386f14f278ebe4533734ddc9608b69e1
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-08-03 22:19:14 +0530

    Validate: Validating the content of Address Book using regex

--- AddressBook.js
+++ AddressBook.js
@@ -1,12 +1,58 @@
 const prompt = require("prompt-sync")();
 console.log("Welcome to address book");
-var firstName = prompt("Enter the first name: ");
-var lastName = prompt("Enter the last name: ");
-var address = prompt("Enter your address: ");
-var city = prompt("Enter your city: ");
-var state = prompt("Enter your state: ");
-var zip = prompt("Enter your ZIP code: ");
-var phoneNumber = prompt("Enter your phone number: ");
-var email = prompt("Enter your Email Id: ");
-var array = [firstName, lastName, address, city, state, zip, phoneNumber, email];
-console.log(array);
+var firstName = RegExp("^[A-Z]{1}[a-z]{2,}$");
+var firstNameTest = firstName.test("Shubom");
+var lastName = RegExp("^[A-Z]{1}[a-z]{2,}$");
+var lastNameTest = lastName.test("Nath");
+var address = RegExp("^[a-z]{4,}$");
+var addressTest = address.test("solung");
+var city = RegExp("^[a-z]{4,}$");
+var cityTest = city.test("pasighat");
+var state = RegExp("^[a-z]{4,}$");
+var stateTest = state.test("arunachal");
+var zip = RegExp("^[0-9]{6}$");
+var zipTest = zip.test("791102");
+var phoneNumber = RegExp("^[0-9]{10}$");
+var phoneNumberTest = phoneNumber.test("8288894252");
+var email = RegExp("^[a-z0-9]{2,}(['.''\\-''+']?)([a-z0-9]*)+@[a-z]{2,}['.'][a-z]{2,}$");
+var emailTest = email.test("shubom.nath88@gmail.com");
+if(firstNameTest==true){
+    console.log("First name is valid");
+}else{
+    console.log("First name is not valid");
+}
+if(lastNameTest==true){
+    console.log("Last name is valid");
+}else{
+    console.log("Last name is not valid");
+}
+if(addressTest==true){
+    console.log("Address is valid");
+}else{
+    console.log("Address is not valid");
+}
+if(cityTest==true){
+    console.log("City is valid");
+}else{
+    console.log("City is not valid")
+}
+if(stateTest==true){
+    console.log("State is valid");
+}else{
+    console.log("State is not valid")
+}
+if(zipTest==true){
+    console.log("Zip code is valid");
+}else{
+    console.log("Zip code is not valid")
+}
+if(phoneNumberTest==true){
+    console.log("Phone number is valid");
+}else{
+    console.log("Phone number is not valid")
+}
+if(emailTest==true){
+    console.log("Email id is valid");
+}else{
+    console.log("Email id is Invalid");
+}
